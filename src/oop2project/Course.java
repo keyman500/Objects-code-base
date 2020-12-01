f8a793474f5213b8a8c08ba24e77e7d914b85078
Dion Recai <dijereo909@gmail.com>
2020-12-01 19:15:22 -0400

    More Course fields and methods added

--- src/oop2project/Course.java
+++ src/oop2project/Course.java
@@ -8,6 +8,19 @@ import java.util.ArrayList;
  * @author Dion Recai
  */
 public class Course implements Requirement {
+	private String courseCode;
+	private String courseTitle;
+	private int credits;
+	private int level;
+	private Requirement prerequisites;
+	private int[] availableSemesters;
+
+	private Course(String courseCode, String courseTitle, int credits, int level) {
+		this.courseCode = courseCode;
+		this.courseTitle = courseTitle;
+		this.credits = credits;
+		this.level = level;
+	}
 
     @Override
     public boolean isFulfilledBy(List<Course> coursesCompleted) {
@@ -15,12 +28,31 @@ public class Course implements Requirement {
     }
 
     @Override
-    public List<Requirement> getMissingComponents(List<Course>) {
+    public List<Requirement> getMissingComponents(List<Course> coursesCompleted) {
     	List<Requirement> missingCourses = new ArrayList<>();
     	if (!coursesCompleted.contains(this)) {
     		missingCourses.add(this);
     	}
     	return missingCourses;
     }
-    
+
+    public boolean hasFulfilledPrerequisites(List<Course> coursesCompleted) {
+    	return this.prerequisites.isFulfilledBy(coursesCompleted);
+    }
+
+    public List<Requirement> getMissingPrerequisites(List<Course> coursesCompleted) {
+    	return this.prerequisites.getMissingComponents(coursesCompleted);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+  		if (o == this) {
+            return true;
+        }
+        if (!(o instanceof Course)) {
+            return false;
+        }
+        Course c = (Complex) o;
+        return this.courseCode == c.courseCode;
+    }
 }
